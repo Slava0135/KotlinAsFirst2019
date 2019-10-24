5788dab0c6a5aec50ea61b8b8002eb3e039dbd90
Slava0135 <40753025+Slava0135@users.noreply.github.com>
2019-10-24 20:25:12 +0300

    fixing again

--- src/lesson9/task2/Matrices.kt
+++ src/lesson9/task2/Matrices.kt
@@ -155,47 +155,61 @@ fun generateRectangles(height: Int, width: Int): Matrix<Int> {
  * 14 17 19 20
  */
 fun generateSnake(height: Int, width: Int): Matrix<Int> {
+
     val matrix = createMatrix(height, width, 0)
-
-    if (height == 1) {
-        for (i in 0 until width) {
-            matrix[0, i] = i + 1
-        }
-        return matrix
-    }
-    if (width == 1) {
-        for (i in 0 until height) {
-            matrix[i, 0] = i + 1
-        }
-        return matrix
-    }
-
     var count = 1
     var position = 0
 
-    while (position < width - 1) {
-        for (i in 0..position) {
-            matrix[i, position - i] = count
-            count++
+    fun leftCorner(arg: Int) {
+        while (position < arg - 1) {
+            for (i in 0..position) {
+                matrix[i, position - i] = count
+                count++
+            }
+            position++
         }
-        position++
     }
 
-    var j = 0
-    while (j < height - position) {
-        for (i in 0..position) {
-            matrix[j + i, width - (i + 1)] = count
-            count++
+    fun rightCorner() {
+        while (position > 0) {
+            for (i in position downTo 1) {
+                matrix[height - i, width - (position - i + 1)] = count
+                count++
+            }
+            position--
         }
-        j++
     }
 
-    while (position > 0) {
-        for (i in position downTo 1) {
-            matrix[height - i, width - (position - i + 1)] = count
-            count++
+    if (height > width) {
+
+        leftCorner(width)
+
+        var j = 0
+        while (j < height - position) {
+            for (i in 0..position) {
+                matrix[j + i, width - (i + 1)] = count
+                count++
+            }
+            j++
         }
-        position--
+
+        rightCorner()
+
+    } else {
+
+        leftCorner(height)
+
+        var j = 0
+        while (j < width - position) {
+            for (i in position downTo 0) {
+                matrix[height - (i + 1), j + i] = count
+                count++
+            }
+            j++
+        }
+
+        rightCorner()
+
     }
     return matrix
 }
